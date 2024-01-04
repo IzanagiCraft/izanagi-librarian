ad9dcde38b68cb482fd5c166748eef215c451c9d
sanguine <152292988+sanguine6660@users.noreply.github.com>
2024-01-04 16:28:43 +0100

    UPDATE libs

--- izanagi-librarian-shared/build.gradle.kts
+++ izanagi-librarian-shared/build.gradle.kts
@@ -2,5 +2,11 @@ dependencies {
     api("com.izanagicraft.messages:message-format:1.0-SNAPSHOT")
     api("com.izanagicraft.storage:data-storage:1.0-SNAPSHOT")
 
-
+    api("redis.clients:jedis:5.1.0")
+    api("org.mongodb:mongodb-driver-legacy:4.7.1")
+    api("com.google.code.gson:gson:2.10.1")
+    api("org.apache.commons:commons-lang3:3.14.0")
+    api("commons-io:commons-io:2.15.1")
+    api("club.minnced:discord-webhooks:0.8.2")
+    api("net.kyori:adventure-text-minimessage:4.14.0")
 }
